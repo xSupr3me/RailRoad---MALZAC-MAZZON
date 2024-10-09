61ea49a5b33a2c72ed30533833e7d6c49fc614fb
Mathis_Mlz <mathismalzac@gmail.com>
2024-10-09 14:08:51 +0200

    Ajout userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,19 @@
 import express from "express";
+import { registerUser, loginUser, getUserProfile, updateUserProfile, deleteUser } from "../controller/userController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+
+router.use(authMiddleware);
+
+router.get("/:id",   getUserProfile);
+
+router.put("/:id", updateUserProfile);
+
+router.delete("/:id", deleteUser);
+
+export default router;
