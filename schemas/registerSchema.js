f50fa21e5e557c6c140491679e8561058c2f78ad
Paul MAZZON* <paul.mazzon@gmail.com>
2024-10-10 10:05:39 +0200

    Ajout username dans le Schema register

--- schemas/registerSchema.js
+++ schemas/registerSchema.js
@@ -1,8 +1,12 @@
+// Shéma d'enregistrement de l'utilisateur avec gestion des erreurs
 import { z } from 'zod';
 
 export const registerSchema = z.object({
-    email: z.string().email('Email non valide'),
 
+    username: z.string().min(3, 'Le nom d utilisateur doit contenir au moins 3 caracteres.'),
+
+    email: z.string().email('Email non valide'),
+    
     password: z.string()
       .min(8, 'Le mot de passe doit contenir au moins 8 caract res.')
       .refine((pass) => /[A-Z]/.test(pass), 'Le mot de passe doit contenir au moins une lettre majuscule.')
