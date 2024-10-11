91441ce1ad8a665b04494218bf6e35378b75f665
Paul MAZZON* <paul.mazzon@gmail.com>
2024-10-11 11:47:10 +0200

    Ajout multer pour upload image et sharp pour le resize mais erreur suppression

--- routes/trainstationRoutes.js
+++ routes/trainstationRoutes.js
@@ -1,15 +1,18 @@
 import express from "express";
 import { addTrainstation, getTrainstationById, getTrainstations, updateTrainstation, deleteTrainstation } from "../controllers/trainstationController.js";
+import { upload } from "../upload.js";
+
 
 const router = express.Router();
 
-router.post("/", addTrainstation);
+
+router.post("/", upload.single('image'), addTrainstation);
+
+router.put("/:id", upload.single('image'), updateTrainstation);
 
 router.get("/", getTrainstations);
 
 router.get("/:id", getTrainstationById);
-
-router.put("/:id", updateTrainstation);
 
 router.delete("/:id", deleteTrainstation);
 
